1ccefddf68e84538a4ccd582667877586f0904db
Edwin Ismail <typhoon_ed@yahoo.com>
2019-06-25 13:53:54 +0200

    addup some data in my resume

--- server/client/src/components/Adress.js
+++ server/client/src/components/Adress.js
@@ -1,25 +1,28 @@
 import React from "react";
 
-const Adress = props => {
+const Adress = () => {
   return (
     <div>
       <p>
         <br />
-        <i className="fa fa-user w3-margin-right w3-large w3-text-red" />Edwin
-        Ismail
+        <i className="fa fa-user w3-margin-right w3-large w3-text-red" />
+        Edwin Ismail
       </p>
       <p>
-        <i className="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-red" />Software
-        Engineer
+        <i className="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-red" />
+        Software Engineer
       </p>
       <p>
-        <i className="fa fa-home fa-fw w3-margin-right w3-large w3-text-red" />Passau,Bavaria,Germany
+        <i className="fa fa-home fa-fw w3-margin-right w3-large w3-text-red" />
+        Passau,Bavaria,Germany
       </p>
       <p>
-        <i className="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-red" />eismail44@gmail.com
+        <i className="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-red" />
+        eismail44@gmail.com
       </p>
       <p>
-        <i className="fa fa-phone fa-fw w3-margin-right w3-large w3-text-red" />+49-151-6608-8240
+        <i className="fa fa-phone fa-fw w3-margin-right w3-large w3-text-red" />
+        +49-1512-4635588
       </p>
       <hr />
     </div>
